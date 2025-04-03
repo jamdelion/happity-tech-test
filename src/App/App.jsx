b720a09e48c6680642b45d600df2a05b2ca4b032
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2025-04-03 20:57:52 +0100

    Extract out ClassListing component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import classData from "../classes.json";
 import "./App.css";
+import { ClassListing } from "./components/ClassListing";
 
 const App = () => {
   const [data, setData] = useState([]);
@@ -13,10 +14,11 @@ const App = () => {
       <h1>Classes in your area</h1>
       <div>
         {/* classes container */}
-        {data.map(({ class_name }) => (
-          <div>
-            <h2 key={class_name}>{class_name}</h2>
-          </div>
+        {data.map((classListing) => (
+          <ClassListing
+            key={classListing.class_name}
+            classListing={classListing}
+          />
         ))}
       </div>
     </div>
---
+++ src/App/components/ClassListing.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ClassListing = ({ classListing }) => {
+  return (
+    <div>
+      <h2>{classListing.class_name}</h2>
+    </div>
+  );
+};
